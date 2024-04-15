6004563e81dd2866bfd34bc7ffcf9e2f2eafd2cc
spiazzano <piazzano.stefano@gmail.com>
2024-04-15 17:11:58 +0200

    change button

--- app/components/ui/buttonCart.tsx
+++ app/components/ui/buttonCart.tsx
@@ -8,10 +8,11 @@ export default function ButtonCart() {
   return (
     <button className="snipcart-checkout w-[38px] h-[38px] ">
       {pathname.includes(searchString) && (
-        <div className="flex items-start">
+        <>
+          {" "}
           <ShoppingCartIcon className=" text-forest" />
-          <span className="snipcart-items-count w-4 h-4 rounded-full bg-yellow-400 text-white text-xs font-semibold -ml-4"></span>
-        </div>
+          {/* <span className="snipcart-items-count w-4 h-4 rounded-full bg-yellow-400 text-white text-xs font-semibold -ml-4"></span> */}
+        </>
       )}
     </button>
   );
