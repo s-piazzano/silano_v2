8a77b85b992ee1c27ca9939c696782af20aba20e
Stefano Piazzano <spiazzano@MacBookPro.silanosrl.it>
2026-04-09 15:57:59 +0200

    aggiunto il token sulla chiamata

--- lib/client.ts
+++ lib/client.ts
@@ -3,6 +3,11 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 const createApolloClient = () => {
   const httpLink = createHttpLink({
     uri: process.env.BASE_URL,
+    headers: {
+      authorization: process.env.STRAPI_API_TOKEN
+        ? `Bearer ${process.env.STRAPI_API_TOKEN}`
+        : "",
+    },
   });
 
   return new ApolloClient({
